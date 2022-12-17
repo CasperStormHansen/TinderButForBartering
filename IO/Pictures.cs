b2d7c21429a72390c431737810f737925749d872
[Casper Storm Hansen] <casper_storm_hansen@hotmail.com>
2022-12-17 22:49:49 +0100

    basic picture functionality added

---
+++ IO/Pictures.cs
@@ -0,0 +1,33 @@
+﻿namespace TinderButForBartering;
+
+public class Pictures
+{
+    public static async Task TakePhoto(Image imageField)
+    {
+        // handle denial of request and errors!
+        await Permissions.RequestAsync<Permissions.Camera>();
+        await Permissions.RequestAsync<Permissions.StorageWrite>();
+        await Permissions.RequestAsync<Permissions.StorageRead>();
+        FileResult photo = await MediaPicker.Default.CapturePhotoAsync();
+        if (photo != null)
+        {
+            Stream sourceStream = await photo.OpenReadAsync(); // a problem was solved by removing "using" from the beginning of this line. Might that cause other problems?
+            imageField.Source = ImageSource.FromStream(() => sourceStream);
+        }
+    }
+
+    public static async Task SelectPhoto(Image imageField)
+    {
+        // handle denial of request and errors!
+        await Permissions.RequestAsync<Permissions.Media>(); //
+        await Permissions.RequestAsync<Permissions.StorageWrite>();
+        await Permissions.RequestAsync<Permissions.StorageRead>();
+        FileResult photo = await MediaPicker.Default.PickPhotoAsync();
+        if (photo != null)
+        {
+            Stream sourceStream = await photo.OpenReadAsync(); // a problem was solved by removing "using" from the beginning of this line. Might that cause other problems?
+            imageField.Source = ImageSource.FromStream(() => sourceStream);
+        }
+    }
+}
+
--- Pages/UserProductDetailsPage.xaml.cs
+++ Pages/UserProductDetailsPage.xaml.cs
@@ -1,4 +1,5 @@
 using static TinderButForBartering.BackendConnection;
+using static TinderButForBartering.Pictures;
 
 namespace TinderButForBartering;
 
@@ -24,5 +25,15 @@ public partial class UserProductDetailsPage : ContentPage
     {
         Product product = new (Titel.Text, Description.Text, Switch.IsToggled);
         await PostProduct(product);
+    }
+
+    async void OnTakePicture_Clicked(object sender, EventArgs e)
+    {
+        await TakePhoto(PrimaryPicture);
+    }
+
+    async void OnSelectPicture_Clicked(object sender, EventArgs e)
+    {
+        await SelectPhoto(PrimaryPicture);
     }
 }
