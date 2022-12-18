c8cb38715c3a8b31a1ad040e17cfcfe86d0735c8
[Casper Storm Hansen] <casper_storm_hansen@hotmail.com>
2022-12-18 22:49:22 +0100

    JSON deserialize

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,4 +1,6 @@
-﻿namespace TinderButForBartering
+﻿using System.Text.Json.Serialization;
+
+namespace TinderButForBartering
 {
     public class Product
     {
@@ -13,6 +15,34 @@
             Description = description;
             RequiresSomethingInReturn = requiresSomethingInReturn;
             PrimaryPictureData = primaryPictureData;
+        }
+    }
+
+    public class ProductWithId
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("productTitle")]
+        public string ProductTitle { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("requiresSomethingInReturn")]
+        public bool RequiresSomethingInReturn { get; set; }
+
+        //[JsonPropertyName("primaryPictureData")]
+        //public byte[] PrimaryPictureData { get; set; }
+
+        [JsonConstructor]
+        public ProductWithId(int id, string title, string description, bool requiresSomethingInReturn)//, byte[] primaryPictureData)
+        {
+            Id = id;
+            ProductTitle = title;
+            Description = description;
+            RequiresSomethingInReturn = requiresSomethingInReturn;
+            //PrimaryPictureData = primaryPictureData;
         }
     }
 }
--- Pages/MyGoodsPage.xaml.cs
+++ Pages/MyGoodsPage.xaml.cs
@@ -1,13 +1,30 @@
+using Newtonsoft.Json;
+using static TinderButForBartering.BackendConnection;
+
 namespace TinderButForBartering;
 
 public partial class MyGoodsPage : ContentPage
 {
-	public MyGoodsPage()
-	{
-		InitializeComponent();
-	}
+    public MyGoodsPage()
+    {
+        InitializeComponent();
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await Update();
+    }
+
     private async void OnUserProductDetails_Clicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new UserProductDetailsPage());
+    }
+
+    async Task Update()
+    {
+        string productsString = await GetProducts();
+        List<Product> products = JsonConvert.DeserializeObject<List<Product>>(productsString);
+        Data.Text = products[0].Description;
     }
 }
