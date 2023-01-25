1979fc794c3a5682250b3f358e4f11434c243e19
[Casper Storm Hansen] <casper_storm_hansen@hotmail.com>
2023-01-25 17:08:26 +0100

    Implement test version of SignalR functionality

--- Pages/MatchPage.xaml.cs
+++ Pages/MatchPage.xaml.cs
@@ -1,9 +1,38 @@
+using Microsoft.AspNetCore.SignalR.Client;
+
 namespace TinderButForBartering;
 
 public partial class MatchPage : ContentPage
 {
+	private readonly HubConnection _connection;
+
 	public MatchPage()
 	{
 		InitializeComponent();
+
+        _connection = new HubConnectionBuilder()
+			.WithUrl("http://10.0.2.2:5045/chat")
+			.Build();
+
+		_connection.On<string>("MessageReceived", (message) =>
+		{
+            chatMessages.Dispatcher.Dispatch(() => {
+                chatMessages.Text += $"{Environment.NewLine}{message}";
+            });
+        });
+
+		Task.Run(() =>
+		{
+			Dispatcher.Dispatch(async () =>
+			await _connection.StartAsync());
+		});
+	}
+
+	private async void OnSendButton_Clicked(object sender, EventArgs e)
+	{
+		await _connection.InvokeCoreAsync("SendMessage", args: new[]
+		{ myChatMessage.Text });
+
+		myChatMessage.Text = String.Empty;
 	}
 }
