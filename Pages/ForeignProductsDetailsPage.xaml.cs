90d7c07673b2f2e76a7ba3df97746d6e0ebda29a
[Casper Storm Hansen] <casper_storm_hansen@hotmail.com>
2023-02-04 10:58:32 +0100

    Add access to product details from MyMatchesPage

---
+++ Pages/ForeignProductsDetailsPage.xaml.cs
@@ -0,0 +1,14 @@
+namespace TinderButForBartering.Pages;
+
+public partial class ForeignProductsDetailsPage : ContentPage
+{
+	public ForeignProductsDetailsPage(Product product)
+	{
+		InitializeComponent();
+
+		ProductTitle.Text = product.Title;
+		Category.Text = Data.Categories[product.Category];
+		Description.Text = product.Description;
+		PrimaryPicture.Source = product.Url;
+    }
+}
--- Pages/MyMatchesPage.xaml.cs
+++ Pages/MyMatchesPage.xaml.cs
@@ -1,3 +1,5 @@
+using TinderButForBartering.Pages;
+
 namespace TinderButForBartering;
 
 public partial class MyMatchesPage : ContentPage
@@ -9,10 +11,17 @@ public partial class MyMatchesPage : ContentPage
         MyMatchesView.ItemsSource = Data.Matches; // Collections in here are arrays and not observablecollections, so updates will not work correctly
     }
 
-    private async void OnMatch_Clicked(object sender, EventArgs e) // to be modified
+    private async void OnMatch_Clicked(object sender, EventArgs e)
     {
         Button button = sender as Button;
         Match match = button.BindingContext as Match;
         await Navigation.PushAsync(new MatchPage(match));
+    }
+
+    private async void OnProduct_Clicked(object sender, EventArgs e)
+    {
+        ImageButton button = sender as ImageButton;
+        Product product = button.BindingContext as Product;
+        await Navigation.PushAsync(new ForeignProductsDetailsPage(product));
     }
 }
